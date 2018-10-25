e55a3f59096b3b880d8ab3eb6de476e93b8b29b7
coordinate <luliyucoordinate@outlook.com>
2018-10-25 18:07:30 +0800

    add some functions for gomoku

--- Gomoku/src/gomoku.cpp
+++ Gomoku/src/gomoku.cpp
@@ -1,6 +1,28 @@
 #include "gomoku.h"
+#include "ui_gomoku.h"
+Gomoku::Gomoku(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Gomoku),
+    _mode(eSingle),
+    _color(Qt::black),
+    _isStarted(false),
+    _isBlocked(false),
+    _canUndo(false)
+{
+    ui->setupUi(this);
+}
 
-Gomoku::Gomoku(QWidget *parent) : QMainWindow(parent)
+Gomoku::~Gomoku()
+{
+    delete ui;
+}
+
+void Gomoku::closeEvent(QCloseEvent *event)
+{
+    emit disconnected();
+}
+
+void Gomoku::SetBlock(bool isBlock)
 {
 
 }
--- Gomoku/src/gomoku.h
+++ Gomoku/src/gomoku.h
@@ -3,15 +3,38 @@
 
 #include <QMainWindow>
 
+namespace Ui {
+class Gomoku;
+}
+
 class Gomoku : public QMainWindow
 {
     Q_OBJECT
 public:
+    enum EMode { eSingle, eNetwork, eAI};
     explicit Gomoku(QWidget *parent = nullptr);
+    ~Gomoku();
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
+private:
+    Ui::Gomoku *ui;
+    EMode _mode;
+    QColor _color;
+    bool _isStarted;
+    bool _isBlocked;
+    bool _canUndo;
+    enum EHostType {eClient, eServer};
+
+private:
+    void SetBlock(bool isBlock);
+    void Initialize();
+
 
 signals:
+    void disconnected();
 
-public slots:
 };
 
 #endif // GOMOKU_H
